595aec198139108a136109bafd6789dc1b1ed95a
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-08-18 18:46:40 +0100

    update to code format

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,5 +4,5 @@
 
 def list_all(mongo_collection):
     """lists all documents in a collection"""
-    document_list = list(mongo_collection.find())
+    document_list = mongo_collection.find()
     return document_list
