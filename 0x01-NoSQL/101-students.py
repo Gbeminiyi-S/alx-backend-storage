f47dadda9d81f026724fa1ba1af9a94f7550cca0
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-19 03:54:03 -0700

    Task 14 - solution

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This modules defines a function `top_students`"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    mapped_students = mongo_collection.aggregate([
+        {
+            "$project": {
+                _id: "_id",
+                name: "name",
+                averageScore: {"$avg": "score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ])
+
+    return mapped_students
